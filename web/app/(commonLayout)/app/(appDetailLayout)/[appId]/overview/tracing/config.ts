6b417da6983c7862c77cc04484a5b5d72a09975a
Nate Mar <nate@arize.com>
2025-03-25 23:32:42 -0700

    update docs link

--- web/app/(commonLayout)/app/(appDetailLayout)/[appId]/overview/tracing/config.ts
+++ web/app/(commonLayout)/app/(appDetailLayout)/[appId]/overview/tracing/config.ts
@@ -4,5 +4,5 @@ export const docURL = {
   [TracingProvider.langSmith]: 'https://docs.smith.langchain.com/',
   [TracingProvider.langfuse]: 'https://docs.langfuse.com',
   [TracingProvider.opik]: 'https://www.comet.com/docs/opik/tracing/integrations/dify#setup-instructions',
-  [TracingProvider.arizePhoenix]: 'https://docs.arize.com/phoenix',
+  [TracingProvider.arizePhoenix]: 'https://docs.arize.com',
 }
